452a1f698c51a4bb069cc9c9d82bdf9089f8c614
jdway <jdway@github.com>
2022-08-08 17:03:28 +0800

    ch5 updated

---
+++ ch4/ch4-1.py
@@ -0,0 +1,158 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as T
+from torchsummary import summary
+import matplotlib.pyplot as plt
+from PIL import Image
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def example_1():
+    """### 4.1.2 卷積神經網路架構
+    #### 範例2：建立一個網路模型輸入大小為28x28x4，並連接一層卷積層，再透過model.summary函數來觀察卷積層所使用的參數數量，如圖4-8所示。最後我們使用上方的參數計算公式，驗證參數數量是否和圖中顯示的1184一致，參數設定如下：
+    * 輸入影像（Input）：28x28x4（長度, 寬度, 深度）。
+    * 填補（Padding）：無。
+    * 步幅（Stride）：1。
+    * 卷積核數量（Kernel number）：32個（kernel_numbers）。
+    * 卷積核大小（Kernel size）：3x3（〖kernel〗_height, kernel_width）。
+    * 偏差值（Bias）：有。
+    """
+
+    class ExampleConvolutionNet(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.relu = F.relu
+            self.conv = nn.Conv2d(3, 32, 3, bias=True)
+
+        def forward(self, x):
+            x = self.relu(self.conv(x))
+            return x
+
+    input_size = (3, 28, 28)
+    net = ExampleConvolutionNet()
+    summary(net, input_size, device=device)
+
+
+def example_2():
+    """### 捲積範例
+    先透過 `!wget` 下載圖片，預設會存在當前的資料夾中
+    """
+    # download lena.jpg from imgur
+    # !wget https://i.imgur.com/gjeDs00.png
+
+    """引入 library"""
+
+    import torchvision.transforms as T
+    from PIL import Image
+
+    """讀取圖片，並將其灰階化、並透過 `unsqueeze` 新增維度以符合網路的輸入"""
+
+    # read input lena image
+    img = Image.open('gjeDs00.png')
+    transform = T.Compose([T.Grayscale(), T.ToTensor()])
+    img = transform(img)
+    img = torch.unsqueeze(img, 0)
+    img_out = img.clone().detach()
+
+    """初始化 kernels"""
+    # initialize convolution kernel
+    kernels = [torch.tensor([[[[1., 0, -1], [0, 0, 0], [-1, 0, 1]]]]),
+               torch.tensor([[[[-1., -1, -1], [-1, 8, -1], [-1, -1, -1]]]]),
+               torch.tensor([[[[0., -1, 0], [-1, 5, -1], [0, -1, 0]]]]),
+               torch.tensor([[[[1., 2, 1], [2, 4, 2], [1, 2, 1]]]]) / 16]
+
+    """分別對每個 kernel 進行捲積，並將其和原圖串聯"""
+
+    for k in kernels:
+        conv_img = F.conv2d(img, k, padding=1)
+        img_out = torch.cat([img_out, conv_img], 3)
+
+    # transpose to fit imshow
+    img_out = torch.permute(img_out, [0, 2, 3, 1])
+
+    import matplotlib.pyplot as plt
+    figure_size = 5
+    plt.figure(figsize=(figure_size * (len(kernels) - 1), figure_size))
+    plt.imshow(img_out[0, :, :, 0], interpolation='nearest', vmin=0, vmax=1, cmap='gray')
+    plt.show()
+
+
+def example_3():
+    # examples for data augmentation
+    dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=T.ToTensor(), download=True)
+    grid_num = 15
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=grid_num ** 2, shuffle=True, num_workers=1)
+
+    """創建 `iterator` 來拿取範例影像"""
+    dataiter = iter(dataloader)
+    images, labels = dataiter.next()
+    """將影像用 torchvision 的工具轉換成 `grid_num` x `grid_num` 的組合圖像，再利用 matplotlib 顯示圖片"""
+    grid_example_img = torchvision.utils.make_grid(images[:grid_num ** 2], grid_num, value_range=(0, 1), normalize=True)
+
+    plt.close()
+    plt.figure(figsize=(10, 10))
+    plt.imshow(np.transpose(grid_example_img, (1, 2, 0)))
+    plt.show()
+
+    """定義 image augmentation，並將其套用至 dataset 中"""
+    transform = T.Compose([
+        T.RandomAffine(degrees=(-5, 5), translate=(0.1, 0.2), scale=(0.9, 1)),
+        T.RandomCrop(size=30),
+        T.Resize(size=32),
+        T.ColorJitter(brightness=(.8, 1.1), contrast=.15, saturation=.15),
+        T.ToTensor(),
+    ])
+
+    dataset_aug = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    dataloader_aug = torch.utils.data.DataLoader(dataset_aug, batch_size=grid_num ** 2, shuffle=True, num_workers=1)
+    dataiter = iter(dataloader_aug)
+    images, labels = dataiter.next()
+    grid_example_img_aug = torchvision.utils.make_grid(images[:grid_num ** 2], grid_num, value_range=(0, 1),
+                                                       normalize=True)
+
+    plt.close()
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5), clear=True)
+    ax[0].imshow(np.transpose(grid_example_img, (1, 2, 0)))
+    ax[0].set_title('Without Augmentation')
+    ax[1].imshow(np.transpose(grid_example_img_aug, (1, 2, 0)))
+    ax[1].set_title('With Augmentation')
+    plt.show()
+
+
+def example_4():
+    """#### 影像增強範例"""
+    img = Image.open('gjeDs00.png')
+    img = T.ToTensor()(img)
+    transform_list = [
+        ['Random Flip', T.RandomHorizontalFlip(p=1.)],
+        ['Random Color', T.ColorJitter(brightness=(.75, 1.25), hue=.1, contrast=.3, saturation=.3)],
+        ['Random Rotation', T.RandomRotation(degrees=(-45, 45))],
+        ['Random Scale', T.RandomAffine(degrees=(0, 0), translate=(0, 0), scale=(0.5, .85))],
+    ]
+
+    plt.close()
+    fig, ax = plt.subplots(len(transform_list), 4, figsize=(12.5, 13.5), clear=True)
+
+    def dim_fixup(img):
+        return torch.permute(img, (1, 2, 0))
+
+    for x in range(4):
+        ax[x, 0].imshow(dim_fixup(img))
+        ax[x, 0].set_title('Original')
+        if x > 0:
+            for index, (trans_name, trans_func) in enumerate(transform_list):
+                ax[index, x].imshow(dim_fixup(trans_func(img)))
+                ax[index, x].set_title(trans_name)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    example_1()
+    example_2()
+    example_3()
+    example_4()
---
+++ ch5/ch5.py
@@ -0,0 +1,169 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms as T
+import matplotlib.pyplot as plt
+from utils_classification import *
+import os
+
+
+class ConvNet(nn.Module):
+    def __init__(self, use_bn=False):
+        super().__init__()
+        place_holder = lambda x: nn.BatchNorm2d(x) if use_bn else nn.Identity()
+        self.layers = nn.ModuleList([
+            nn.Conv2d(3, 64, (3, 3)),
+            place_holder(64),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2)),
+            nn.Conv2d(64, 128, (3, 3)),
+            place_holder(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, (3, 3)),
+            place_holder(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 128, (3, 3)),
+            place_holder(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 64, (3, 3)),
+            place_holder(64),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, 64),
+            place_holder(64),
+            nn.ReLU(),
+            nn.Dropout(.5),
+            nn.Linear(64, 10, bias=False),
+            nn.Softmax(-1)
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+# define initialization
+def init_weights(method='xavier'):
+    def init(m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+            if method == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight)
+            elif method == 'he':
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif method == 'noraml':
+                torch.nn.init.normal_(m.weight, std=1)
+    return init
+
+
+def image_transform_loader(img_size, with_aug=False, p=.5, flip_h=True, flip_v=False,
+                           color=False, contrast=False, sharpness=False, crop_rand=False,
+                           crop_center=False, blur=False, rotate=False):
+    transform_list = [T.ToTensor()]
+    if with_aug:
+        if flip_h:
+            transform_list += [T.RandomHorizontalFlip(p)]
+        if flip_v:
+            transform_list += [T.RandomVerticalFlip(p)]
+        if color:
+            transform_list += [T.ColorJitter(brightness=.5, hue=.3)]
+        if contrast:
+            transform_list += [T.RandomAutocontrast()]
+        if sharpness:
+            transform_list += [T.RandomAdjustSharpness(sharpness_factor=2)]
+        if blur:
+            transform_list += [T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))]
+        if crop_rand:
+            to_size = int(img_size * .8)
+            transform_list += [T.RandomCrop(size=(to_size, to_size))]
+        if crop_center:
+            transform_list += [T.CenterCrop(size=img_size)]
+        if rotate:
+            transform_list += [T.RandomRotation(degrees=15)]
+    transform_list += [T.Resize(size=img_size)]
+    transform_list += [T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    return T.Compose(transform_list)
+
+
+def forward_hook(net, index):
+    def hook(model, input, output):
+        net.layer_output[index] = output.detach().cpu().numpy()
+    return hook
+
+
+def vshow_hist(in_list):
+    len_l = len(in_list)
+    plt.figure(figsize=(len_l * 2, 5))
+    for i, layer_output in enumerate(in_list):
+        plt.subplot(1, len(in_list), i + 1)
+        plt.title(str(i + 1) + "-layer")
+        if i != 0: plt.yticks([], [])
+        plt.hist(layer_output.flatten(), 30, range=[0, 1])
+    plt.show()
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    batch_size = 128
+    cpu_num = 6 if os.cpu_count() > 6 else os.cpu_count()
+    if os.name == 'nt':
+        # cpu num > 1 will slowing down in windows, not sure why
+        cpu_num = 1
+
+    transform = image_transform_loader(32)
+    transform_aug = image_transform_loader(32, with_aug=True, flip_v=True, contrast=True, sharpness=True,
+                                           crop_rand=True, rotate=True)
+    dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
+    dataset_aug = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_aug, download=True)
+    d_len = len(dataset)
+    indices = np.arange(d_len)
+    np.random.shuffle(indices)
+    train_indices = indices[:int(d_len * .7)]
+    valid_indices = indices[int(d_len * .7):]
+
+    train_subset = torch.utils.data.Subset(dataset_aug, train_indices)
+    valid_subset = torch.utils.data.Subset(dataset_aug, valid_indices)
+
+    loader_train = torch.utils.data.DataLoader(train_subset, batch_size=batch_size,
+                                               shuffle=True, num_workers=cpu_num, pin_memory=True)
+    loader_valid = torch.utils.data.DataLoader(valid_subset, batch_size=batch_size, shuffle=False,
+                                               num_workers=cpu_num, pin_memory=True)
+
+    dataiter = iter(loader_train)
+    images, labels = dataiter.next()
+
+    from torch.utils.tensorboard import SummaryWriter
+    model_dir = 'models'
+    try:
+        os.mkdir(model_dir)
+    except:
+        print(f'dir already existed: {model_dir}')
+
+    epochs = 200
+    lr = 3e-4
+
+    model_seq = zip(
+        ('xavier', 'he', 'normal', 'xavier'),
+        (False, False, False, True),
+        ('model_xavier', 'model_he', 'model_normal', 'model_bn')
+    )
+    cnt = 0
+    for init_method, use_bn, name in model_seq:
+        model = ConvNet(use_bn=use_bn)
+        model.apply(init_weights(init_method))
+        cnt += 1
+        print(f'Training model: {name}')
+        model.cuda()
+        model.train()
+        writer = SummaryWriter(os.path.join(model_dir, name))
+        loader_train = loader_train
+        fit(epochs, lr, model, loader_train, loader_valid, writer)
+        model.eval()
+        writer.add_graph(model, torch.rand_like(images, device=device))
+        writer.close()
+
+
+if __name__ == '__main__':
+    main()
