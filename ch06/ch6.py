01f4a6cb67e123e6f281e06c42478f1124b7a02e
jdway <jdw.davidjaw@gmail.com>
2023-09-05 08:44:20 +0800

    add ch06

---
+++ ch06/ch6.py
@@ -0,0 +1,104 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+from utils import ModelWrapper, image_transform_loader
+from network import CustomModel, ConvNet
+from tqdm import tqdm
+import os
+
+
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    batch_size = 128
+    cpu_num = 6 if os.cpu_count() > 6 else os.cpu_count()
+    if os.name == 'nt':
+        cpu_num = 0
+
+    img_size = 64
+    transform = image_transform_loader(img_size)
+    transform_aug = image_transform_loader(img_size, with_aug=True, rotate=True, flip_h=True, contrast=True,
+                                           sharpness=True)
+
+    target_dataset = 1  # 0: CIFAR100, 1: CIFAR10
+    if target_dataset == 0:
+        dataset = torchvision.datasets.CIFAR100(root='./data', train=True, transform=transform, download=True)
+        dataset_aug = torchvision.datasets.CIFAR100(root='./data', train=True, transform=transform_aug, download=True)
+        class_num = 100
+    else:
+        dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
+        dataset_aug = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_aug, download=True)
+        class_num = 10
+
+    d_len = len(dataset)
+    # 透過 random_split 將 dataset 分成 training set 和 validation set
+    # 這邊我們透過設定 np.random.seed 來確保每次執行時都會得到相同的 training set 和 validation set
+    np.random.seed(9527)
+    # 創建一個由 0 到 d_len 的 index list, 並透過 np.random.shuffle 來將其打亂
+    indices = np.arange(d_len)
+    np.random.shuffle(indices)
+    np.random.seed()
+    # 將打亂後的 indices 依照 7:3 的比例分成 training set 和 validation set
+    train_indices = indices[:int(d_len * .7)]
+    valid_indices = indices[int(d_len * .7):]
+    # 使用 Subset 來建立 training set 和 validation set, 並使用 DataLoader 來建立 dataloader
+    train_subset = torch.utils.data.Subset(dataset_aug, train_indices)
+    valid_subset = torch.utils.data.Subset(dataset, valid_indices)
+    loader_train = DataLoader(train_subset, batch_size=batch_size,
+                              shuffle=True, num_workers=cpu_num, pin_memory=True)
+    loader_valid = DataLoader(valid_subset, batch_size=batch_size, shuffle=False,
+                              num_workers=cpu_num, pin_memory=True)
+    # 將一組 batch 的資料取出, 等待之後 tensorboard 進行網路架構視覺化使用
+    dataiter = iter(loader_train)
+    images, labels = next(dataiter)
+    images = images.to(device)
+
+    model_dir = 'models'
+    os.makedirs(model_dir, exist_ok=True)
+    weight_dir = 'weights'
+    os.makedirs(weight_dir, exist_ok=True)
+
+    model_custom = CustomModel(class_num).to(device)
+    model_conv = ConvNet(use_bn=True, class_num=class_num).to(device)
+    model_seq = zip(
+        ('ConvNet', 'CustomModel'),  # 模型名稱
+        (model_conv, model_custom)   # 模型
+    )
+    for model_name, model in model_seq:
+        if model_name != 'CustomModel':
+            continue
+        writer = SummaryWriter(os.path.join(model_dir, model_name))
+        # 向前傳遞一次, 讓模型權重初始化
+        model(images)
+        # 使用 tensorboard 紀錄模型架構
+        model.eval()
+        writer.add_graph(model, images)
+
+        epochs = 50
+        lr = 1e-3
+        running_loss_train, running_accu_train, running_loss_valid, running_accu_valid = 0, 0, 0, 0
+        model_wrapper = ModelWrapper(model, device, lr, checkpoint_path=os.path.join(weight_dir, f'{model_name}.pt'))
+        for i in range(epochs):
+            # 使用 tqdm 來顯示訓練進度
+            postfix = {'loss': running_loss_train, 'accu': running_accu_train} if i > 0 else {}
+            tqdm_loader_train = tqdm(loader_train, desc=f'Epoch {i + 1}/{epochs} [Train]', dynamic_ncols=True, postfix=postfix)
+            # 進行一個 epoch 的訓練並更新 running loss 和 running accuracy
+            loss_train, accu_train = model_wrapper.train_epoch(tqdm_loader_train, writer, i)
+            running_loss_train = loss_train
+            running_accu_train = accu_train
+
+            postfix = {'loss': running_loss_valid, 'accu': running_accu_valid} if i > 0 else {}
+            tqdm_loader_valid = tqdm(loader_valid, desc=f'Epoch {i + 1}/{epochs} [Validation]', dynamic_ncols=True, postfix=postfix)
+            loss_val, accu_val = model_wrapper.eval(tqdm_loader_valid, writer, i)
+            running_loss_valid = loss_val
+            running_accu_valid = accu_val
+            if i % 5 == 0:
+                model_wrapper.save_checkpoint()
+                model_wrapper.record_parameters(writer, i)
+        writer.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ch06/utils.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms as T
+
+
+def classification_accuracy(logits, y):
+    pred = torch.argmax(logits, dim=1)
+    return (pred == y).float().mean() * 100
+
+
+class ModelWrapper(object):
+    def __init__(
+            self,
+            model: nn.Module,
+            device: torch.device,
+            lr: float = 1e-3,
+            optimizer: torch.optim.Optimizer = torch.optim.AdamW,
+            criterion: nn.Module = nn.CrossEntropyLoss,
+            metric: callable = classification_accuracy,
+            checkpoint_path: str = None,
+            load_model: bool = False,
+            load_optimizer: bool = False,
+    ):
+        self.model = model
+        if load_model:
+            self.load_checkpoint(load_optimizer=load_optimizer)
+        self.device = device
+        self.optimizer = optimizer(model.parameters(), lr=lr)
+        self.criterion = criterion()
+        self.metric = metric
+        self.checkpoint_path = checkpoint_path
+        self.best_loss = float('inf')
+
+    def forward(self, x):
+        return self.model(x)
+
+    def save_checkpoint(self):
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+        }, self.checkpoint_path)
+
+    def load_checkpoint(self, load_optimizer=True):
+        import os
+        assert self.checkpoint_path is not None and os.path.exists(self.checkpoint_path), \
+            'checkpoint_path is None or not exists'
+        checkpoint = torch.load(self.checkpoint_path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        if load_optimizer:
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    def get_loss(self, x, y, is_logit=True):
+        logit = x if is_logit else self.forward(x)
+        return self.criterion(logit, y), logit
+
+    def get_accuracy(self, x, y, is_logit=True):
+        logit = x if is_logit else self.forward(x)
+        return self.metric(logit, y)
+
+    def record_parameters(self, writer, epoch):
+        for name, param in self.model.named_parameters():
+            writer.add_histogram(name, param, epoch)
+
+    def train_epoch(self, data_loader, writer=None, epoch=None):
+        train_loss, train_accu = 0, 0
+        self.model.train()
+        for batch in data_loader:
+            self.optimizer.zero_grad()
+            x, y = batch
+            x = x.to(self.device)
+            y = y.to(self.device)
+            loss, logit = self.get_loss(x, y, is_logit=False)
+            loss.backward()
+            self.optimizer.step()
+
+            train_loss += loss.item()
+            train_accu += self.get_accuracy(logit, y).item()
+
+        train_loss /= len(data_loader)
+        train_accu /= len(data_loader)
+        if writer is not None:
+            writer.add_scalar('loss/train', train_loss, epoch)
+            writer.add_scalar('accu/train', train_accu, epoch)
+        return train_loss, train_accu
+
+    def eval(self, data_loader, writer=None, epoch=None):
+        valid_loss, valid_accu = 0, 0
+        self.model.eval()
+        with torch.no_grad():
+            for batch in data_loader:
+                x, y = batch
+                x = x.to(self.device)
+                y = y.to(self.device)
+                loss, logit = self.get_loss(x, y, is_logit=False)
+                valid_loss += loss.item()
+                valid_accu += self.get_accuracy(logit, y).item()
+
+        valid_loss /= len(data_loader)
+        valid_accu /= len(data_loader)
+        if writer is not None:
+            writer.add_scalar('loss/valid', valid_loss, epoch)
+            writer.add_scalar('accu/valid', valid_accu, epoch)
+
+        if valid_loss < self.best_loss:
+            self.best_loss = valid_loss
+            self.save_checkpoint()
+
+        return valid_loss, valid_accu
+
+
+def image_transform_loader(img_size, with_aug=False, p=.5, flip_h=False, flip_v=False,
+                           color=False, contrast=False, sharpness=False, crop_rand=False,
+                           crop_center=False, blur=False, rotate=False):
+    transform_list = [T.ToTensor()]
+    if with_aug:
+        if flip_h:
+            transform_list += [T.RandomHorizontalFlip(p)]
+        if flip_v:
+            transform_list += [T.RandomVerticalFlip(p)]
+        if color:
+            transform_list += [T.ColorJitter(brightness=.5, hue=.3)]
+        if contrast:
+            transform_list += [T.RandomAutocontrast(p)]
+        if sharpness:
+            transform_list += [T.RandomAdjustSharpness(sharpness_factor=2, p=p)]
+        if blur:
+            transform_list += [T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))]
+        if crop_rand:
+            to_size = int(img_size * .8)
+            transform_list += [T.RandomCrop(size=(to_size, to_size))]
+        if crop_center:
+            transform_list += [T.CenterCrop(size=img_size)]
+        if rotate:
+            transform_list += [T.RandomRotation(degrees=5)]
+    transform_list += [T.Resize(size=img_size)]
+    transform_list += [T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    return T.Compose(transform_list)
+
+
+
