6ffa2b62f0367af4315e5b7f887426604fb039b2
jdway <jdw.davidjaw@gmail.com>
2023-09-08 12:31:27 +0800

    Update Lab 08

---
+++ Lab 08 Load Pretrained Model from Pytorch/README.md
@@ -0,0 +1,10 @@
+# Lab 8: Load Pretrained Model from Pytorch
+
+This Lab simply shows how to load a pretrained model from Pytorch hub, and use `tensorboard` to visualize the model. 
+The model is loaded and tested on a dog image from the pytorch official github repository.
+In this lab, `Inception v3`, `ResNet-50`, `MobileNet-v3` are tested.
+
+## Core concepts
+* Basic usage of `torch.hub`
+
+
--- Lab 08 Load Pretrained Model from Pytorch/ch8.py
+++ Lab 08 Load Pretrained Model from Pytorch/ch8.py
@@ -1,28 +1,14 @@
-import os
-import numpy as np
 import torch
-import torch.nn as nn
 import torch.hub
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
-import torchvision.models.inception as inc
 from torchinfo import summary
 from PIL import Image
 
 
-def chap821():
+def run_pretrained():
     # 定義 device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # 加載 InceptionV3 模型，並載入 device
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
-    model.to(device)
-    # 設置為評估模式
-    model.eval()
-
-    # 定義 input shape 並印出 model
-    input_size = (1, 3, 299, 299)
-    summary(model, input_size)
 
     # 從 pytorch 官方 Github 上下載 "狗" 類別的圖片, 存到 'dog.jpg'
     import urllib.request
@@ -35,6 +21,7 @@ def chap821():
     img_input = Image.open(filename)
 
     # 預處理函式的定義: 改變圖片大小、截取圖片中間區域、張量化、標準化
+    # 注意這邊的預處理需要依照預訓練模型的預處理方式進行
     transform = torchvision.transforms.Compose([
         torchvision.transforms.Resize(299),
         torchvision.transforms.CenterCrop(299),
@@ -48,25 +35,37 @@ def chap821():
     image = transform(img_input).unsqueeze(0)
     image = image.to(device)
 
-    # 將圖片送入模型進行預測
-    output = model(image)
+    for model_name in ['inception_v3', 'resnet50', 'mobilenet_v3_small']:
+        # 加載 InceptionV3 模型，並載入 device
+        model = torch.hub.load('pytorch/vision:v0.10.0', model_name, weights='IMAGENET1K_V1')
 
-    # 創建一個SummaryWriter，將網路架構的可視化送入TensorBoard中
-    writer = SummaryWriter('runs/inception_v3')
-    writer.add_graph(model, image)
-    writer.close()
+        # 設置為評估模式
+        model.eval()
 
-    # 輸出預測結果
-    top_k = torch.topk(torch.nn.functional.softmax(output, dim=1), k=5)
-    classes = top_k.indices[0].tolist()
-    probabilities = top_k.values[0].tolist()
+        # 定義 input shape 並印出 model
+        print(f'{model_name} 網路架構：')
+        input_size = (1, 3, 299, 299)
+        summary(model, input_size)
 
-    print(f'Top 5 classes are: {classes}')
-    print(f'Estimated probs: {probabilities}')
+        # 將圖片送入模型進行預測
+        output = model(image)
+
+        # 創建一個SummaryWriter，將網路架構的可視化送入TensorBoard中
+        writer = SummaryWriter(f'runs/{model_name}')
+        writer.add_graph(model, image)
+        writer.close()
+
+        # 輸出預測結果
+        top_k = torch.topk(torch.nn.functional.softmax(output, dim=1), k=5)
+        classes = top_k.indices[0].tolist()
+        probabilities = top_k.values[0].tolist()
+
+        print(f'Top 5 classes are: {classes}')
+        print(f'Estimated probs: {probabilities}')
 
 
 def main():
-    chap821()
+    run_pretrained()
 
 
 if __name__ == '__main__':
