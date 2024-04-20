19f28cd789df3056ed08a83f12feaf3932dbad71
jdway <jdw.davidjaw@gmail.com>
2024-04-20 14:23:36 +0800

    update amp

--- Lab 14 Advanced CNN/main.py
+++ Lab 14 Advanced CNN/main.py
@@ -9,6 +9,8 @@ from data_loader import OxfordPetsDataset, JointTransform, map_trimap
 from network import SegConvNextV2
 from tqdm import tqdm
 import os
+import random
+import numpy as np
 
 
 def get_confusion_matrix_elements(predicted, target, class_index):
@@ -19,69 +21,85 @@ def get_confusion_matrix_elements(predicted, target, class_index):
     predicted_binary = (predicted == class_index).float()
     target_binary = (target == class_index).float()
     # 透過二元陣列計算 TP, FP, FN, TN
+    # TP: 正確的正樣本 (預測為正樣本, 真實為正樣本)
     TP = torch.sum(predicted_binary * target_binary).item()
+    # FP: 錯誤的正樣本 (預測為正樣本, 真實為負樣本)
     FP = torch.sum(predicted_binary * (1 - target_binary)).item()
+    # FN: 錯誤的負樣本 (預測為負樣本, 真實為正樣本)
     FN = torch.sum((1 - predicted_binary) * target_binary).item()
+    # TN: 正確的負樣本 (預測為負樣本, 真實為負樣本)
     TN = torch.sum((1 - predicted_binary) * (1 - target_binary)).item()
     return TP, FP, FN, TN
 
 
 def performance_metrics(predicted, target, nc=3):
     """
-    Compute performance metrics for each class
-    :param predicted: Predicted segmentation mask
-    :param target: Ground truth segmentation mask
-    :param nc: Number of classes
+    本函式將對於每一個類別計算 IoU, Precision, Recall, 並計算所有類別的平均值
+    :param predicted: 預測的分割 mask
+    :param target: 真實的分割 mask
+    :param nc: 類別數量
     :return: mIoU, IoUs, mPrecision, mRecall
     """
     ious = []
     precisions = []
     recalls = []
-
     for class_index in range(nc):
         # 藉由 get_confusion_matrix_elements 函式取得 TP, FP, FN, TN
         TP, FP, FN, _ = get_confusion_matrix_elements(predicted, target, class_index)
         # 計算 IoU (Intersection over Union, 重疊聯集比)
         iou = TP / (TP + FP + FN) if (TP + FP + FN) != 0 else 0
         ious.append(iou)
-        # 計算 Precision (準確率)
+        # 計算 Precision (精確率, 即正預測中有多少是正樣本)
         precision = TP / (TP + FP) if (TP + FP) != 0 else 0
         precisions.append(precision)
-        # 計算 Recall (召回率)
+        # 計算 Recall (召回率, 即正樣本中有多少被預測為正樣本)
         recall = TP / (TP + FN) if (TP + FN) != 0 else 0
         recalls.append(recall)
-
     # m 代表 mean, 即所有類別的平均值
     mIoU = sum(ious) / len(ious)
     mPrecision = sum(precisions) / len(precisions)
     mRecall = sum(recalls) / len(recalls)
-
     return mIoU, ious, mPrecision, mRecall
 
 
-def train(model, dataloader, criterion, optimizer, write_img=True, denorm_func=None, device=None):
+def train_epoch(model, dataloader, criterion, optimizer, scaler, write_img=True, denorm_func=None, device=None):
     model.train()
     running_loss = 0.0
     # 使用 tqdm 顯示訓練進度
     tqdm_loader = tqdm(dataloader, desc='Training')
     for i, data in enumerate(tqdm_loader):
+        # 解構 batch data, 並將資料移至 device
         batch_img, batch_trimap, batch_class, batch_sp, batch_breed = data
         batch_img = batch_img.to(device)
         batch_trimap = batch_trimap.to(device)
+        # 將 optimizer 梯度歸零
         optimizer.zero_grad()
-        outputs = model(batch_img)
-        loss = criterion(outputs, batch_trimap)
-        loss.backward()
-        optimizer.step()
+        # 混合精度訓練
+        with torch.cuda.amp.autocast():
+            # 前向傳遞
+            outputs = model(batch_img)
+            # 計算 loss
+            loss = criterion(outputs, batch_trimap).mean()
+        # 透過梯度縮放器將 loss 放大, 並進行反向傳遞
+        scaler.scale(loss).backward()
+        # 截斷過大的梯度避免梯度爆炸
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)
+        # 透過梯度縮放器對放大的梯度進行反縮放
+        scaler.unscale_(optimizer)
+        # 透過 optimizer 更新參數
+        scaler.step(optimizer)
+        # 更新梯度縮放器
+        scaler.update()
+        # 紀錄 loss, 並顯示在 tqdm 進度條上
         running_loss += loss.item()
         tqdm_loader.set_postfix({'Loss': loss.item()})
 
     if write_img:
+        # 取得預測結果
         outputs = torch.argmax(outputs, 1)
-        # convert mask to image
+        # 將預測結果與真實結果轉換成 3 通道的 mask
         outputs = map_trimap(outputs, False) / 255.
         batch_trimap = map_trimap(batch_trimap, False) / 255.
-        # convert single channel mask to 3 channel
         outputs = outputs.unsqueeze(1).repeat(1, 3, 1, 1)
         batch_trimap = batch_trimap.unsqueeze(1).repeat(1, 3, 1, 1)
         # denormalize the image
@@ -100,85 +118,119 @@ def validate(model, dataloader, criterion, device=None):
     tqdm_loader = tqdm(dataloader, desc='Validation')
     preds, targets = [], []
     for i, data in enumerate(tqdm_loader):
+        # 解構 batch data, 並將資料移至 device
         batch_img, batch_trimap, batch_class, batch_sp, batch_breed = data
         batch_img = batch_img.to(device)
         batch_trimap = batch_trimap.to(device)
         with torch.no_grad():
+            # 前向傳遞
             outputs = model(batch_img)
-            loss = criterion(outputs, batch_trimap)
+            # 計算 loss
+            loss = criterion(outputs, batch_trimap).mean()
             running_loss += loss.item()
-
+            # 紀錄預測結果與真實結果
             preds.append(torch.argmax(outputs, 1).cpu())
             targets.append(batch_trimap.cpu())
-
+        # 顯示 loss 在 tqdm 進度條上
         tqdm_loader.set_postfix({'Loss': loss.item()})
-
+    # 透過 performance_metrics 函式計算 mIoU, IoUs, mPrecision, mRecall
     miou, ious, mprecision, mrecall = performance_metrics(torch.cat(preds), torch.cat(targets))
-
+    # 回傳 loss, mIoU, IoUs, mPrecision, mRecall
     return running_loss / len(dataloader), miou, ious, mprecision, mrecall
 
 
+def seed_everything(seed=9527):
+    # 固定隨機種子, 藉此讓整體訓練過程可復現
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+
 if __name__ == '__main__':
+    # 固定隨機種子
+    seed_everything(9527)
+    # 設定訓練參數
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
-
     batch_size = 128
     learning_rate = 0.001
     num_epochs = 50
-
+    # 載入資料集, 設定 transform
     dataset_path = r'..\..\dataset\Oxford-IIIT Pet'
     valid_transform = transforms.Resize((64, 64))
     train_transform = transforms.Compose([
+        # 訓練時進行隨機水平翻轉
         transforms.RandomHorizontalFlip(),
-        transforms.RandomAffine(degrees=(-10, 10), translate=(0.1, 0.1)),  # random shift
+        # 訓練時進行隨機旋轉與平移
+        transforms.RandomAffine(degrees=(-10, 10), translate=(0.1, 0.1)),
         transforms.Resize((64, 64)),
     ])
+    # 設定色彩相關的 transform
     train_transform_color = transforms.ColorJitter(contrast=0.2)
+    # 建立訓練與驗證資料集
     train_transform_joint = JointTransform(train_transform)
     valid_transform_joint = JointTransform(valid_transform)
     train_dataset = OxfordPetsDataset(dataset_path, 'trainval.txt', train_transform_joint, train_transform_color)
     valid_dataset = OxfordPetsDataset(dataset_path, 'test.txt', valid_transform_joint)
 
-    n_workers = 3 if os.name == 'nt' else 8
+    # 建立 DataLoader
+    n_workers = 2 if os.name == 'nt' else 8
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=n_workers)
-    valid_loader = DataLoader(valid_dataset, batch_size=int(batch_size * 1.5), shuffle=True, num_workers=n_workers)
+    valid_loader = DataLoader(valid_dataset, batch_size=int(batch_size * 1.5), shuffle=True, num_workers=0)
     denorm_func = train_dataset.denorm
 
     network_names = ['ResNet', 'ResNeXt', 'ConvNeXt-v2', 'CSPNet-SPPF', 'CSPNeXt-SPPF']
-    for network_type in range(3, 5):
+    for network_type in range(5):
+        # 初始化模型, 損失函數, 優化器
         model = SegConvNextV2(3, network_type).to(device)
         train_loss = []
         val_loss = []
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss(reduction='none')
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        # 初始化 tensorboard writer
         writer = SummaryWriter(f'runs/Seg-{network_names[network_type]}')
         model.to(device)
-
-        # Record model graph
+        # 將模型結構寫入 tensorboard
         dummy_input = torch.rand((1, 3, 64, 64)).to(device)
         writer.add_graph(model, dummy_input)
-
+        # 創建混合精度訓練所需的梯度縮放器
+        scaler = torch.cuda.amp.GradScaler()
+        # 開始訓練
         for epoch in range(num_epochs):
-            epoch_train_loss, debug_img = train(model, train_loader, criterion, optimizer, True, denorm_func, device)
+            # 模型訓練函式
+            epoch_train_loss, debug_img = train_epoch(model, train_loader, criterion, optimizer, scaler, True, denorm_func, device)
+            # 模型驗證函式
             epoch_val_loss, *valid_metric = validate(model, valid_loader, criterion, device)
+            # 紀錄 loss
             train_loss.append(epoch_train_loss)
             val_loss.append(epoch_val_loss)
+            # 顯示訓練與驗證 loss
             print(f"Epoch {epoch + 1} - Training Loss: {epoch_train_loss:.4f}, Validation Loss: {epoch_val_loss:.4f}")
-
             if epoch % 5 == 0:
-                # Record histogram of weights
+                # 每 5 個 epoch 將模型參數寫入 tensorboard
                 for name, param in model.named_parameters():
                     if param.requires_grad and "bias" not in name:
                         writer.add_histogram(name, param, epoch)
+            # 將 debug image 寫入 tensorboard
             writer.add_image('debug_image', debug_img, epoch)
+            # 將 loss 寫入 tensorboard
             writer.add_scalar('train/loss', epoch_train_loss, epoch)
             writer.add_scalar('valid/loss', epoch_train_loss, epoch)
-
+            # 將 mIoU, mPrecision, mRecall 寫入 tensorboard
             miou, ious, mprecision, mrecall = valid_metric
             writer.add_scalar('valid/mIoU', miou, epoch)
             writer.add_scalar('valid/mPrecision', mprecision, epoch)
             writer.add_scalar('valid/mRecall', mrecall, epoch)
             for i, iou in enumerate(ious):
+                # 將每一個類別的 IoU 寫入 tensorboard
                 writer.add_scalar(f'valid/IoU-{i}', iou, epoch)
 
         writer.close()
--- Lab 14 Advanced CNN/network.py
+++ Lab 14 Advanced CNN/network.py
@@ -14,30 +14,40 @@ class ResNetBlk(nn.Module):
             group=False
     ):
         super(ResNetBlk, self).__init__()
+        # ResNet 與 ResNeXt 的差異在於 ResNeXt 使用 group convolution
         g = 4 if group else 1
-
+        # ResNet block 的第一個 convolution, 這裡使用 1x1 convolution 來降低維度
         self.conv1 = nn.Sequential(
+            # 採用 pre-normalization 的方式, 依序進行 batch norm, activation, 1x1 convolution
             nn.BatchNorm2d(d_i) if batch_norm else nn.Identity(),
             act(inplace=True),
             nn.Conv2d(d_i, d_o // 4, kernel_size=1, stride=1, bias=False, groups=g),
         )
+        # ResNet block 的第二個 convolution, 這裡使用 3x3 convolution 來提取特徵
         self.conv2 = nn.Sequential(
+            # 一樣採用 pre-normalization 的方式, 依序進行 batch norm, activation, 3x3 convolution
             nn.BatchNorm2d(d_o // 4) if batch_norm else nn.Identity(),
             act(inplace=True),
             nn.Conv2d(d_o // 4, d_o // 4, kernel_size=3, stride=2 if down_sample else 1, padding=1, bias=False, groups=g),
         )
+        # ResNet block 的第三個 convolution, 這裡使用 1x1 convolution 來提升維度
         self.conv3 = nn.Conv2d(d_o // 4, d_o, kernel_size=1, stride=1, groups=g)
 
         self.identity = nn.Sequential(
+            # 如果 down_sample 為 True, 則使用 2x2 的 MaxPooling 來進行下採樣
             nn.MaxPool2d(2, 2) if down_sample else nn.Identity(),
+            # 如果輸入與輸出的維度不同, 則使用 1x1 convolution 來調整維度
             nn.Conv2d(d_i, d_o, 1, 1, groups=g) if d_i != d_o else nn.Identity(),
         )
 
     def forward(self, x):
+        # 先將輸入 x 透過 identity block 進行轉換, 儲存到 identity 變數中
         identity = self.identity(x)
+        # 依序通過三個 convolution
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
+        # 將輸入 x 與 identity 相加, 即為 ResNet block 的輸出
         x = x + identity
         return x
 
@@ -45,101 +55,154 @@ class ResNetBlk(nn.Module):
 class SPPF(nn.Module):
     def __init__(self, d_i, d_o, depth=2):
         super().__init__()
+        """ 
+        Spatial Pyramid Pooling Fusion (SPPF), 
+        其概念是將不同尺度的特徵圖進行 Cross-receptive field 的特徵提取,
+        再透過 Concatenate 的方式將特徵圖合併
+        """
+        # 隱藏層維度為輸出維度的一半
         d_h = d_o // 2
+        # 定義 SPPF 的深度
+        self.depth = depth
+        # 1x1 convolution 降低輸入維度
         self.conv1 = nn.Sequential(
             nn.Conv2d(d_i, d_h, 1, 1),
             nn.BatchNorm2d(d_h),
             nn.SiLU(),
         )
+        # 3x3 convolution 提取特徵, 輸入維度為 (depth + 1) * d_h
         self.conv2 = nn.Sequential(
             nn.Conv2d((depth + 1) * d_h, d_o, 3, 1, 1),
             nn.BatchNorm2d(d_o),
             nn.SiLU(),
         )
+        # MaxPooling, 注意這裡的輸出因為 stride 為 1, padding 為 2, 因此特徵圖大小不變
         self.m = nn.MaxPool2d(5, 1, 2)
-        self.depth = depth
 
     def forward(self, x):
+        # 透過 1x1 convolution 降低輸入維度
         xs = [self.conv1(x)]
+        # 透過 MaxPooling 提取不同尺度的特徵, 共提取 depth 次
         xs.extend(self.m(xs[-1]) for _ in range(self.depth))
+        # 先進行 concat 整合不同 receptive field 的特徵, 再透過 3x3 convolution 提取特徵
         return self.conv2(torch.cat(xs, dim=1))
 
 
 class Bottleneck(nn.Module):
     def __init__(self, d_i, d_o, group=None):
+        """
+        Bottleneck 計算區塊類似於 ResNet 的基本區塊, 也可選擇是否使用 group convolution
+        此區塊進一步簡化了 ResNet block 的結構, 只使用了一個 1x1 convolution 以及一個 3x3 convolution
+        """
         super().__init__()
+        # 如果 group 不為 None, 則使用 group convolution
         g = group if group else 1
+        # 隱藏層維度為輸出維度的一半
         d_h = d_o // 2
+        # 1x1 convolution 降低輸入維度, 使用 post-norm 的方式進行 batch norm, activation
         self.conv1 = nn.Sequential(
             nn.Conv2d(d_i, d_h, 1, 1, groups=g),
             nn.BatchNorm2d(d_h),
             nn.SiLU(),
         )
+        # 3x3 convolution 提取特徵, 使用 post-norm 的方式進行 batch norm, activation
         self.conv2 = nn.Sequential(
             nn.Conv2d(d_h, d_o, 3, 1, 1, groups=g),
             nn.BatchNorm2d(d_o),
             nn.SiLU(),
         )
+        # 如果輸入與輸出的維度不同, 則使用 1x1 convolution 來調整維度
         self.residual = nn.Identity() if d_i == d_o else nn.Conv2d(d_i, d_o, 1, 1, groups=g)
 
     def forward(self, x):
+        # 透過 1x1 convolution 降低輸入維度, 再透過 3x3 convolution 提取特徵, 最後將輸入的 residual 與特徵相加
         return self.residual(x) + self.conv2(self.conv1(x))
 
 
 class C2fBlock(nn.Module):
     def __init__(self, d_i, d_o, depth=2, group=None):
+        """
+        C2fBlock 計算區塊類似於一種可自訂的 ResNet 的基本區塊, 是由 YOLOv7 所提出的一種區塊
+        其在確保了梯度流的情況下, 進一步針對特徵提取與跨感受野的特徵融合進行了優化
+        """
         super().__init__()
+        # 如果 group 不為 None, 則使用 group convolution
         g = group if group else 1
+        # 隱藏層維度為輸出維度的一半
         h_dim = d_o // 2
+        # 1x1 convolution 降低輸入維度, 使用 post-norm 的方式進行 batch norm, activation
         self.p1 = nn.Sequential(
             nn.Conv2d(d_i, h_dim * 2, 1, 1, groups=g),
             nn.BatchNorm2d(h_dim * 2),
             nn.SiLU(),
         )
+        # 1x1 convolution 提取特徵, 使用 post-norm 的方式進行 batch norm, activation
         self.p2 = nn.Sequential(
             nn.Conv2d((2 + depth) * h_dim, d_o, 1, 1, groups=g),
             nn.BatchNorm2d(d_o),
             nn.SiLU(),
         )
+        # 進行 depth 次 Bottleneck 的特徵提取
         self.d = nn.ModuleList([Bottleneck(h_dim, h_dim, group) for _ in range(depth)])
 
     def forward(self, x):
+        # 透過 1x1 convolution 降低輸入維度, 並將其於 channel 維度上拆分為兩部分
         xs = list(self.p1(x).chunk(2, dim=1))
+        # 進行 depth 次 Bottleneck 的特徵提取, 並將特徵保存於 xs 中
         xs.extend(m(xs[-1]) for m in self.d)
+        # 將不同尺度的特徵進行 concat, 再透過 1x1 convolution 整合特徵
         return self.p2(torch.cat(xs, dim=1))
 
 
 class ConvNeXtV2Block(nn.Module):
     def __init__(self, dim):
         super().__init__()
+        # 深度可分離卷積層，用於在每個通道內進行局部空間特徵提取。
         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
+        # 局部響應標準化層，用於對每個通道的特徵進行標準化。
         self.norm = LayerNorm(dim, eps=1e-6)
-        self.pwconv1 = nn.Linear(dim, 4 * dim)  # pointwise/1x1 convs, implemented with linear layers
+        # 第一個 1x1 卷積（使用全連接層實現），用於擴展特徵維度。
+        self.pwconv1 = nn.Linear(dim, 4 * dim)
+        # 非線性轉換激活函數
         self.act = nn.GELU()
+        # ConvNeXtv2 提出的全局響應標準化層，用於整合全局信息。
         self.grn = GRN(4 * dim)
+        # 第二個 1x1 卷積（使用全連接層實現），用於壓縮特徵維度。
         self.pwconv2 = nn.Linear(4 * dim, dim)
         self.drop_path = nn.Identity()
 
     def forward(self, x):
+        # 將 residual 保存起來
         input = x
+        # 使用定義的網路架構進行特徵提取
         x = self.dwconv(x)
         x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
         x = self.norm(x)
+        # 1x1 卷積等價於全連接層
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.grn(x)
         x = self.pwconv2(x)
         x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
-
+        # 加入 residual
         x = input + self.drop_path(x)
         return x
 
 
 class LayerNorm(nn.Module):
     def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        """
+        層正則化實現。
+        參數:
+            normalized_shape (int): 要正則化的特徵維度。
+            eps (float): 避免除零錯誤的小值。
+            data_format (str): 數據格式，支持 'channels_last' 或 'channels_first'。
+        """
         super().__init__()
+        # 初始化正則權重和偏置
         self.weight = nn.Parameter(torch.ones(normalized_shape))
         self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        # 初始化超參數
         self.eps = eps
         self.data_format = data_format
         if self.data_format not in ["channels_last", "channels_first"]:
@@ -148,8 +211,10 @@ class LayerNorm(nn.Module):
 
     def forward(self, x):
         if self.data_format == "channels_last":
+            # 若資料格式為 channels_last，則對最後一個維度進行正則化
             return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
         elif self.data_format == "channels_first":
+            # 若資料格式為 channels_first，則對第二個維度進行正則化
             u = x.mean(1, keepdim=True)
             s = (x - u).pow(2).mean(1, keepdim=True)
             x = (x - u) / torch.sqrt(s + self.eps)
@@ -159,13 +224,25 @@ class LayerNorm(nn.Module):
 
 class GRN(nn.Module):
     def __init__(self, dim):
+        """
+        全局響應標準化 (GRN) 的實現。
+        參數:
+            dim (int): 輸入特徵的通道數。
+        屬性:
+            gamma (nn.Parameter): 縮放係數，用於校正標準化後的特徵。
+            beta (nn.Parameter): 平移係數，用於校正標準化後的特徵。
+        """
         super().__init__()
+        # 初始化縮放係數和平移係數
         self.gamma = nn.Parameter(torch.zeros(1, 1, 1, dim))
         self.beta = nn.Parameter(torch.zeros(1, 1, 1, dim))
 
     def forward(self, x):
+        # 全局范數
         Gx = torch.norm(x, p=2, dim=(1, 2), keepdim=True)
+        # 正規化
         Nx = Gx / (Gx.mean(dim=-1, keepdim=True) + 1e-6)
+        # 套用可學習的縮放和平移
         return self.gamma * (x * Nx) + self.beta + x
 
 
@@ -177,7 +254,11 @@ class SegConvNextV2(nn.Module):
     ):
         super(SegConvNextV2, self).__init__()
         if network_type < 2:
+            # 若 network_type 小於 2，則使用 ResNet block 進行特徵提取
+            # 若 network_type 為 1，則使用 group convolution, 即為 ResNeXt
             grouped = network_type == 1
+            # Encoder 是一連串的 ResNet/ResNeXt block 所組成, 每個 resolution 會創建新的 nn.Sequential,
+            # 這樣可以更方便的拿到不同 resolution 的特徵進行 U-net 的結構串接
             self.encoder = nn.Sequential(
                 nn.Sequential(
                     nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
@@ -197,11 +278,14 @@ class SegConvNextV2(nn.Module):
                 ),
             )
         elif network_type == 2:
+            # ConvNeXtV2 架構, 一樣是透過 nn.Sequential 來串接不同 resolution 的區塊
             self.encoder = nn.Sequential(
                 nn.Sequential(
                     nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
                 ),
                 nn.Sequential(
+                    # 這邊要注意由於 ConvNeXtV2Block 並沒有撰寫關於 dimension mapping 或 down sampling 的部分,
+                    # 因此要手動透過 nn.Conv2d 或 nn.MaxPool2d 來進行 channel mapping 或 down sampling
                     ConvNeXtV2Block(64),
                     nn.Conv2d(64, 128, kernel_size=1, stride=1),
                     nn.MaxPool2d(2, 2),
@@ -221,7 +305,9 @@ class SegConvNextV2(nn.Module):
                 ),
             )
         elif network_type >= 3:
+            # 這邊的 group 用來控制是否使用 group convolution
             group = 4 if network_type == 4 else 1
+            # Encoder 是一連串的 C2fBlock 所組成, 每個 resolution 也是創建新的 nn.Sequential
             self.encoder = nn.Sequential(
                 nn.Sequential(
                     nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
@@ -246,7 +332,7 @@ class SegConvNextV2(nn.Module):
                     nn.MaxPool2d(2, 2),
                 ),
             )
-
+        # 所有不同 network_type 的 encoder 都會接上相同的 decoder 作為相對公平的比較
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2, padding=0),
             nn.BatchNorm2d(256),
